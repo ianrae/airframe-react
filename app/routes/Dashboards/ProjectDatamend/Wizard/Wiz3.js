17abaf3b55baa2b361b6fb14831e76061a8cfb61
Ian Rae <ianrae88@gmail.com>
2020-12-13 10:29:20 -0500

    inline form

--- app/routes/Dashboards/ProjectDatamend/Wizard/Wiz3.js
+++ app/routes/Dashboards/ProjectDatamend/Wizard/Wiz3.js
@@ -23,28 +23,64 @@ import {
     DropdownToggle,
     DropdownMenu,
     DropdownItem,
-    UncontrolledDropdown
+    UncontrolledDropdown,
+    CardTitle,
+    CustomInput,
+    FormText    
 } from './../../../../components';
 
 
 
 const WizardStep3 = () => (
     <Row>
-        <Col md={12}>
-            <div>
-                <h3 className="mb-4">
-                    Fields
-                </h3>
-                <p className="pb-3">
-                    Below is a sample page for your cart , Created using pages design UI Elementes.
-                </p>
-                <small>
-                    Invoice are issued on the date of despatch. 
-                    Payment terms: Pre-orders: within 10 days of invoice date with 4% discount, 
-                    from the 11th to the 30th day net. Re-orders: non-reduced stock items are payable net after 20 days.
-                </small>
-            </div>
-        </Col>
+                <Col lg={ 12 }>
+                    <Card className="mb-3">
+                        <CardBody>
+                            <CardTitle tag="h6" className="mb-4">
+                                Forms Inline: Preview Example
+                                <span className="small ml-1 text-muted">
+                                    #2.01
+                                </span>
+                            </CardTitle>
+                            { /* START Form */}
+                            <Form inline>
+                                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                    <Input type="text" name="text" id="enterName" placeholder="Enter Name..." />
+                                </FormGroup>
+                                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                    <InputGroup>
+                                        <InputGroupAddon addonType="prepend">
+                                            <i className="fa fa-key fa-fw" />
+                                        </InputGroupAddon>
+                                        <Input type="password" name="password" placeholder="Password..." />
+                                    </InputGroup>
+                                </FormGroup>
+                                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                                    <InputGroup>
+                                        <InputGroupAddon addonType="prepend">
+                                            Country
+                                        </InputGroupAddon>
+                                        <CustomInput type="select" id="country-selector-3" name="customSelect">
+                                            <option value="">Select...</option>
+                                            <option>United Kingdom</option>
+                                            <option>United States</option>
+                                            <option>Canada</option>
+                                            <option>Australia</option>
+                                            <option>New Zeland</option>
+                                        </CustomInput>
+                                    </InputGroup>
+                                </FormGroup>
+                                <FormGroup>
+                                    <CustomInput type="checkbox" id="rememberMe" label="Remember Me" inline />
+                                </FormGroup>
+                                <Button color="primary">
+                                    Submit
+                                </Button>
+                            </Form>
+                            { /* END Form */}
+                        </CardBody>
+                    </Card>
+                </Col>
     </Row>
 );
 
